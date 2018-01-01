77b339dc07f5a28ccba2e35b5ba67d3dc9c3b7e7
aun <aun.sira@gmail.com>
2018-01-01 16:01:02 +0700

    More coverage for Payload

--- client_test.go
+++ client_test.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"errors"
+	"net/http"
 	"testing"
 
 	"github.com/datawowio/k-sequencing-go/actions"
+	"github.com/datawowio/k-sequencing-go/config"
 	a "github.com/stretchr/testify/assert"
 )
 
 const (
 	TestProjectKey = "A7sRxaQKxo2hRQzNwkk5Qqx4"
 )
+
+type mockPayload struct {
+	ID       string
+	CustomID string
+}
+
+func (*mockPayload) Endpoint() (string, string, string) {
+	return config.LocalAPI, "method", "path"
+}
+
+func (m *mockPayload) Payload(endpoint, method, path string) (*http.Request, error) {
+	return nil, errors.New("Mock error for payload testing")
+}
 
 func TestNewClient(t *testing.T) {
 	c := NewClient(TestProjectKey)
@@ -41,4 +57,14 @@ func TestClient_InvalidCall(t *testing.T) {
 
 	e := c.Call(closedQuestion, getImage)
 	a.EqualError(t, e, "Error occurred from API side")
+}
+
+func TestClient_InvalidPayload(t *testing.T) {
+	c := NewClient(TestProjectKey)
+	a.NotNil(t, c)
+
+	m := &mockPayload{}
+	e := c.Call(nil, m)
+	a.NotNil(t, e)
+	a.EqualError(t, e, "Mock error for payload testing")
 }
