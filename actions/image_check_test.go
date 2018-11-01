a35166ea5e2afb69c8cb384e812de246cf34a8d4
aun <aun.sira@gmail.com>
2018-11-01 20:58:42 +0700

    test: Add unit tests

---
+++ actions/image_check_test.go
@@ -0,0 +1,76 @@
+package actions
+
+import (
+	"testing"
+
+	"github.com/datawowio/k-sequencing-go/config"
+	a "github.com/stretchr/testify/assert"
+)
+
+func TestGetImageCheckEndpoint(t *testing.T) {
+	g := &GetImageCheck{}
+	endpoint, method, path := g.Endpoint()
+	a.NotNil(t, endpoint)
+	a.Equal(t, config.KSeqAPIURL, endpoint)
+	a.NotNil(t, method)
+	a.Equal(t, "GET", method)
+	a.NotNil(t, path)
+	a.Equal(t, "/api/images/check", path)
+}
+
+func TestGetListImageCheckEndpoint(t *testing.T) {
+	g := &GetImageChecks{}
+	endpoint, method, path := g.Endpoint()
+	a.NotNil(t, endpoint)
+	a.Equal(t, config.KSeqAPIURL, endpoint)
+	a.NotNil(t, method)
+	a.Equal(t, "GET", method)
+	a.NotNil(t, path)
+	a.Equal(t, "/api/images/check", path)
+}
+
+func TestPostImageCheckEndpoint(t *testing.T) {
+	p := &PostImageCheck{}
+	endpoint, method, path := p.Endpoint()
+	a.NotNil(t, endpoint)
+	a.Equal(t, config.KSeqAPIURL, endpoint)
+	a.NotNil(t, method)
+	a.Equal(t, "POST", method)
+	a.NotNil(t, path)
+	a.Equal(t, "/api/images/check", path)
+}
+
+func TestGetImageCheckPayload(t *testing.T) {
+	g := &GetImageCheck{
+		ID: "5a44671ab3957c2ab5c33326",
+	}
+	endpoint, method, path := g.Endpoint()
+	req, _ := g.Payload(endpoint, method, path)
+	a.Equal(t, req.URL.Path, path+"/5a44671ab3957c2ab5c33326")
+}
+
+func TestGetListImageCheckPayload(t *testing.T) {
+	g := &GetImageChecks{
+		ID:   "5a44671ab3957c2ab5c33326",
+		Item: "1",
+		Page: "5",
+	}
+	endpoint, method, path := g.Endpoint()
+	req, _ := g.Payload(endpoint, method, path)
+	queryValues := req.URL.Query()
+	a.Equal(t, g.ID, queryValues.Get("id"))
+	a.Equal(t, g.Item, queryValues.Get("per_page"))
+	a.Equal(t, g.Page, queryValues.Get("page"))
+}
+
+func TestPostImageCheckPayload(t *testing.T) {
+	p := &PostImageCheck{
+		Data:           "https://assets-cdn.github.com/images/modules/open_graph/github-mark.png",
+		PostbackURL:    "http://someUrl.url",
+		PostbackMethod: "POST",
+		CustomID:       "custom_id",
+	}
+	endpoint, method, path := p.Endpoint()
+	req, _ := p.Payload(endpoint, method, path)
+	a.NotNil(t, req)
+}
